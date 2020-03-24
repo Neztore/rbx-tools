2dad87ad8ad3c4bd9950f83ac88a9000b207faa9
Neztore <josh@muir.xyz>
2020-03-24 23:59:27 +0000

    Update

--- RemoteManager.lua
+++ RemoteManager.lua
@@ -12,26 +12,20 @@ https://nezto.re
 getRemote: Simply fetches a remote, returns null if it does not exist.
 addRemote: Create a remote. If it already exists, returns it. Also can create remoteFunctions
 fetchRemote: Fetches a remote. If it does not exist, creates it.
-bindEvent: Binds to the given event. Accepts a name, FunctionToBind & Permission function. Perm func is optional and should return true to allow or false to deny.
-fireEvent: Fire an event from the client to the server
+bind: Binds to the given event or function. Accepts a name, FunctionToBind & Permission function. Perm func is optional and should return true to allow or false to deny.
+fire: Fire an event or a function from the client to the server
 
 
 Remote names are case insensitive.
 
-TODO: Once I work out how to have variable number of params, move to that.
-Version: 1.0.0
+Version: 1.1.0
 --]]
 local runService = game:GetService("RunService")
 
 local module = {}
 
-module.warn = function(msg, arg2)
-	if arg2 then
-		warn("RemoteManager: "..msg, arg2)
-	else
-		warn("RemoteManager: "..msg)
-	end
-	
+module.warn = function(o, ...)
+	warn("RemoteManager: "..o, ...)
 end
 
 module.getRemote = function(name)
@@ -66,48 +60,86 @@ module.addRemote = function(name, isFunction)
 	end
 end
 
-module.fetchRemote = function(name)
+module.fetchRemote = function(name, isFunction)
 	name = string.lower(name)
 	
-	local remote = script:WaitForChild(name, 10)
+	local remote = script:WaitForChild(name, 0.5)
 	if remote then
 		return remote
 	else
-		return module.addRemote(name)
+		if not runService:IsClient() then
+			return module.addRemote(name, isFunction)
+		else
+			warn("Cannot create (fetchEvent) as on client.")
+			return nil
+		end
+		
 	end
 end
 
 -- Server side only, realistically.
-module.bindEvent = function(name, funcToBind, permFunc)
-	local remote = module.fetchRemote(name)
-	remote.OnServerEvent:connect(function(plr, one, two, three, four, five, six, seven, eight, nine, ten)
+
+
+-- Supports binding both functions and events. 
+module.bind = function(name, funcToBind, permFunc, isFunction)
+	local remote = module.fetchRemote(name, isFunction)
+	
+	local isEvent = remote:IsA("RemoteEvent")
+
+	local function bindFunc (plr, ...)
 		if permFunc ~= nil then
-			if permFunc(plr, one, two, three, four, five, six, seven, eight, nine, ten) then
-			
-				funcToBind(plr, one, two, three, four, five, six, seven, eight, nine, ten)
+			-- not clean but theres no other way to return "all" values from it.
+			local allowed, a,b,c,d,f,g = permFunc(plr, ...) 
+			if allowed then
+				return funcToBind(plr, ...)
 			else
-				module.warn("Player "..plr.Name.." tried to fire "..name.. " but is not allowed.")
+				if not b then module.warn("Player "..plr.Name.." tried to fire "..name.. " but is not allowed.") end
+				return allowed, a, b, c,d,f,g
 			end
 		else
-			funcToBind(plr, one, two, three, four, five, six, seven, eight, nine, ten)
+			return funcToBind(plr, ...)
 		end
-	end)
+	end
+	
+	if isEvent then
+		remote.OnServerEvent:connect(bindFunc)
+	else
+		remote.OnServerInvoke = bindFunc
+	end
+	
+	
 	
 	return remote
 end
 
+
 -- A shortcut
-module.fireEvent = function(name, one, two, three, four, five, six, seven, eight, nine, ten)
-	local remote = module.fetchRemote(name)
+module.fire = function(name, ...)
+	local remote = module.getRemote(name)
 	if not remote then
 		module.warn("Failed to fire - Remote does not exist.")
 		return false
 	end
+	local isEvent = remote:IsA("RemoteEvent")
+	
 	if runService:IsClient() then
-		remote:FireServer(one, two, three, four, five, six, seven, eight, nine, ten)
+		if isEvent then
+			return remote:FireServer(...)
+		else
+			return remote:InvokeServer(...)
+		end
+		
 	else
+		
 		module.warn("Couldn't fire event from server. This function is only for the client.")
+		return nil, "Cannot be used on server!"
 	end
 	
 end
+
+-- Old aliases
+module.bindEvent = module.bind
+module.fireEvent = module.fire
+
+	
 return module
